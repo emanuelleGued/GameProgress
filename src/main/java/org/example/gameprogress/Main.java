5de15f91d2781d4985326d9fc1674447591a65d1
emanuelleGued <manugguedes1701@gmail.com>
2025-02-17 19:23:59 -0300

    add main

---
+++ src/main/java/org/example/gameprogress/Main.java
@@ -0,0 +1,10 @@
+package org.example.gameprogress;
+
+
+import org.example.gameprogress.ui.GameProgressUI;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameProgressUI();
+    }
+}
